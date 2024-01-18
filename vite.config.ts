dd8e35a3b8af1ce62d5f32a2610a5b7e752b7385
Tom Gao <tom@tomgao.cc>
2024-01-18 23:29:09 +0800

    chore: change tsup to vite build

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  build: {
+    target: ['node16', 'chrome87', 'firefox78', 'safari14', 'edge88'],
+    lib: {
+      entry: 'src/index.ts',
+      fileName: 'index',
+      name: 'Tomjs',
+      formats: ['es', 'cjs', 'iife'],
+    },
+    minify: false,
+    rollupOptions: {
+      output: {
+        exports: 'named',
+      },
+    },
+  },
+  plugins: [dts({ rollupTypes: true })],
+});
