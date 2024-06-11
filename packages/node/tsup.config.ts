c40e1b3b28bbdbcb7066d3a2a2c8039fd13d2ef1
Tom Gao <tom@tomgao.cc>
2024-06-11 17:41:11 +0800

    chore: use tsup replace vite

---
+++ packages/node/tsup.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig(options => {
+  const isDev = !!options.watch;
+
+  return {
+    entry: ['src/index.ts'],
+    format: ['esm', 'cjs'],
+    target: 'node16',
+    shims: true,
+    clean: true,
+    dts: true,
+    sourcemap: isDev,
+    splitting: true,
+    minifyWhitespace: !isDev,
+  };
+});
---
+++ packages/utils/tsup.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig(options => {
+  const isDev = !!options.watch;
+
+  return {
+    entry: ['src/index.ts'],
+    format: ['esm', 'cjs', 'iife'],
+    globalName: 'Tomjs',
+    target: 'node16',
+    shims: true,
+    clean: true,
+    dts: true,
+    sourcemap: isDev,
+    splitting: true,
+    minifyWhitespace: !isDev,
+  };
+});
